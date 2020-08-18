5884a7148a912dc930495037ffda0f446d162945
ParisQZhang <parischang97@gmail.com>
2020-08-18 10:30:14 -0400

    Adde teacher and student controllers

--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const classesController = require('./controllers/classes');
-
+const studentsController = require('./controllers/students');
+const teachersController = require('./controllers/teachers');
 // CLASSES - TEACHER
 // Create class
 router.post('/classes', classesController.createClass);
@@ -12,5 +13,12 @@ router.delete('/classes', classesController.deleteClass);
 router.get('/classes', classesController.getClasses);
 
 // TODO: Dealing with unfound pages
+router.get('/students', studentsController.getStudents);
+
+router.post('/students', studentsController.createStudent);
+
+router.get('/teachers', teachersController.getTeachers);
+
+router.post('/teachers', teachersController.createTeacher);
 
 module.exports = router;
