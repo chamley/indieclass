9a6c94259ae522707cff8fa41b189ae7d63adae6
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2020-08-18 13:39:37 +0300

    FEAT(Server): Get classes by teacher controller function working

--- server/controllers/classes.js
+++ server/controllers/classes.js
@@ -27,14 +27,16 @@ exports.createClass = async (req, res) => {
 // Removes a new database entry in classes table
 exports.deleteClass = async (req, res) => {
   try {
-    db.class.findOne({
+    const cls = await db.class.findOne({
       where: {
         class_id: req.body.class_id
       }
     })
-    .then(cls=>console.log(cls))
-    .then(cls => cls.destroy())
-    .catch(err=>res.send("This record does not exist"));
+    if (!cls) {
+      res.send("Record not found")
+    } else {
+      cls.destroy();
+    }
     res.sendStatus(204);
   } catch (error) {
     console.log(error); // eslint-disable-line no-console
@@ -43,9 +45,9 @@ exports.deleteClass = async (req, res) => {
   }
 }
 
-// Get Classes
-// Looks up all class ID's in the teacher-class binding table that relate to the teacher ID, and then returns the class entries from the class table for relating the the class IDs
-exports.getClasses = async (req, res) => {
+// Get All classes
+// Get's all classes in db
+exports.getAllClasses = async (req, res) => {
   try {
     const classes = await db.class.findAll();
     res.send(classes);
@@ -56,4 +58,23 @@ exports.getClasses = async (req, res) => {
     res.json(error);
   }
 }
+
+// Get teacher's classes
+// Looks up all class ID's in the teacher-class binding table that relate to the teacher ID, and then returns the class entries from the class table for relating the the class IDs
+exports.getClasses = async (req, res) => {
+  try {
+    const classes = await db.class.findAll({
+      where: {
+        teacher_id: req.params.teacher_id
+      }
+    });
+    res.send(classes);
+    res.status(200);
+  } catch (error) {
+    console.log(error); // eslint-disable-line no-console
+    res.status(500);
+    res.json(error);
+  }
+}
+
 
