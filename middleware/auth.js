2c36535384315c05a320c2e68354eab7e0c9c406
ParisQZhang <parischang97@gmail.com>
2020-08-20 12:07:14 -0400

    Added google OAuth, connected to frontend, log in and logout working

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = async (req, res, next) => {
+  const authHeaders = req.headers['authorization'];
+  if (!authHeaders) return res.sendStatus(403);
+  const token = authHeaders.split(' ')[1];
+  try {
+    //need to verify
+    const decoded = jwt.decode(token);
+    const email = decoded.email;
+    const firstname = decoded.given_name;
+    const lastname = decoded.family_name;
+    const newUser = { email, firstname, lastname };
+    req.user = newUser;
+    next();
+  } catch (error) {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = authMiddleware;
