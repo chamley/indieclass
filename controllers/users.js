57a4155643257669d24619dca84eaea1730fa475
ParisQZhang <parischang97@gmail.com>
2020-08-25 20:28:05 -0400

    Map search working

--- controllers/users.js
+++ controllers/users.js
@@ -39,9 +39,9 @@ exports.assignUserToClass = async (req, res) => {
       where: { class_id: req.body.class_id },
     });
     if (req.user_id === assignedClass.teacher_id) {
-      res.status(401).json("You cannot sign up for your own class!");
+      res.status(401).json('You cannot sign up for your own class!');
     } else if (assignedClass.signedup === assignedClass.limit) {
-      res.status(401).json("The class is full!");
+      res.status(401).json('The class is full!');
     } else {
       await db.student_class.create({
         user_id: req.user_id,
@@ -64,7 +64,7 @@ exports.assignUserToClass = async (req, res) => {
   }
 };
 
-exports.upgradeToTeacher = async (req, res) => {
+exports.editBio = async (req, res) => {
   try {
     const teacher = await db.user.findOne({
       where: { user_id: req.user_id },
@@ -73,9 +73,7 @@ exports.upgradeToTeacher = async (req, res) => {
       res.status(404);
       res.json('Record not found');
     } else {
-      if (teacher.isteacher === true)
-        res.json('You have  already signed up as a teacher');
-      else teacher.isteacher = true;
+      teacher.bio = req.body.bio;
       await teacher.save();
     }
     res.json(teacher);
@@ -127,6 +125,7 @@ exports.profile = async (req, res) => {
     const encodedUser = {
       firstname: newUser.dataValues.firstname,
       lastname: newUser.dataValues.lastname,
+      bio: newUser.dataValues.bio,
       token: token,
     };
     console.log('token created on signin', token);
@@ -134,86 +133,5 @@ exports.profile = async (req, res) => {
   } catch (error) {
     console.log(error); // eslint-disable-line no-console
     res.status(404).json({ error, message: 'Resource not found' });
-  }
-};
-
-exports.createTeacher = async (req, res) => {
-  try {
-    const existingUser = await db.user.findOne({
-      where: { user_id: req.user_id },
-    });
-
-    if (!existingUser) {
-      res.status(404);
-      res.json('Your are not found');
-    } else if (existingUser.isteacher === false) {
-      existingUser.isteacher = true;
-      existingUser.save();
-    }
-
-    const existingTeacher = await db.teacher.findOne({
-      where: { user_id: req.user_id },
-    });
-
-    if (existingTeacher) {
-      res.status(404);
-      res.json('You are already a teacher!');
-    } else {
-      const extraTeacherInfo = await db.teacher.create({
-        user_id: req.user_id,
-        ...req.body,
-      });
-      const teacherinUsers = await db.user.findOne({
-        where: { user_id: req.user_id },
-      });
-      res.status(201);
-      res.json({
-        firstname: teacherinUsers.firstname,
-        lastname: teacherinUsers.lastname,
-        bio: extraTeacherInfo.bio,
-        talent_field: extraTeacherInfo.talent_field,
-      });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(404).json({ err, message: 'Resource not found' });
-  }
-};
-
-exports.getTeacher = async (req, res) => {
-  try {
-    const teacher = await db.teacher.findOne({
-      where: { user_id: res.body }
-    });
-    if (!teacher) {
-      res.json("This teacher does not exist");
-      res.status(404);
-    } else {
-      res.json(teacher);
-      res.status(200);
-    }  
-  } catch (error) {
-    console.log(error); // eslint-disable-line no-console
-    res.status(500);
-    res.json(error);
-  }
-};
-
-exports.getTeacher = async (req, res) => {
-  try {
-    const teacher = await db.teacher.findOne({
-      where: { user_id: res.body }
-    });
-    if (!teacher) {
-      res.json("This teacher does not exist");
-      res.status(404);
-    } else {
-      res.json(teacher);
-      res.status(200);
-    }  
-  } catch (error) {
-    console.log(error); // eslint-disable-line no-console
-    res.status(500);
-    res.json(error);
   }
 };
