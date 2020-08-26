881f2fd654242d9ec000dd428bdfc57454d2eb6c
ParisQZhang <parischang97@gmail.com>
2020-08-25 21:08:20 -0400

    Changed NavBar icons & explore page color

--- client/App.js
+++ client/App.js
@@ -1,21 +1,22 @@
-/* 
-Comments:
-*/
-
 import { StatusBar } from 'expo-status-bar';
 import React, { useState, Profiler } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Redux from 'redux';
 import { Provider, connect, useSelector } from 'react-redux';
-
 import { store } from './store/store';
-
+import { FontAwesome } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 
-import { addMyClassDB, getMyClassesDB, getExploreClassesDB } from './store/actions';
+import {
+  addMyClassDB,
+  getMyClassesDB,
+  getExploreClassesDB,
+} from './store/actions';
 import ExploreStackScreen from './routes/ExploreStack';
 import Explore from './screens/Explore';
 import MyClasses from './screens/MyClasses';
@@ -24,7 +25,6 @@ import AuthSignin from './screens/Signin';
 import MapView from './components/mapView';
 
 export default function App() {
-  
   return (
     <Provider store={store}>
       <ConnectedWrapper />
@@ -33,16 +33,15 @@ export default function App() {
 }
 
 const Tab = createBottomTabNavigator();
-const backgroundImage = { uri: "./assets/images/background.jpg" } 
+const backgroundImage = { uri: './assets/images/background.jpg' };
 
 // This component is simpy a child for Provided wrapper that will run through the connected function
 const Wrapper = function (props) {
-  
   // const user = useSelector(state => state.user)
 
   return (
     <NavigationContainer>
-        <MyTabs/>
+      <MyTabs />
     </NavigationContainer>
   );
 };
@@ -54,8 +53,7 @@ const ConnectedWrapper = connect(mapStateToProps, {
 })(Wrapper);
 
 function MyTabs() {
-
-  const user = useSelector(state => state.user)
+  const user = useSelector((state) => state.user);
 
   const logout = async () => {
     setSignedIn(false);
@@ -70,25 +68,29 @@ function MyTabs() {
         options={{
           tabBarLabel: 'Profile',
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color="red" size={size} />
+            <MaterialCommunityIcons
+              name="face-profile"
+              size={24}
+              color="black"
+            />
           ),
         }}
       />
-      )
-    } else {
-      profileOrLoginTab = (
+    );
+  } else {
+    profileOrLoginTab = (
       <Tab.Screen
         name="Sign In"
         component={AuthSignin}
         options={{
           tabBarLabel: 'Sign In',
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="menu" color="red" size={size} />
+            <Feather name="log-in" size={24} color="black" />
           ),
         }}
       />
-      )
-    } 
+    );
+  }
 
   return (
     <Tab.Navigator
@@ -104,7 +106,7 @@ function MyTabs() {
         options={{
           tabBarLabel: 'Explore',
           tabBarIcon: ({ color, size }) => (
-            <AntDesign name="search1" size={24} color="blue" />
+            <FontAwesome5 name="map-marked-alt" size={24} color="black" />
           ),
         }}
       />
@@ -114,7 +116,7 @@ function MyTabs() {
         options={{
           tabBarLabel: 'My Classes',
           tabBarIcon: ({ color, size }) => (
-            <AntDesign name="table" size={24} color="green" />
+            <FontAwesome name="calendar-o" size={24} color="black" />
           ),
         }}
       />
@@ -129,7 +131,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
 });
 
 function mapStateToProps(state) {
