aa595aac183ea23f6a0acbfb1ea4c5861f05dd16
ParisQZhang <parischang97@gmail.com>
2020-08-26 14:14:51 -0400

    Added styling for teacher profile and edit profile page

--- client/teacher-screens/TeacherEditProfile.js
+++ client/teacher-screens/TeacherEditProfile.js
@@ -1,25 +1,18 @@
 import React, { useState } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  ScrollView,
-} from 'react-native';
-import { Dimensions } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
+import { Dimensions, TouchableOpacity } from 'react-native';
 
 import { TextInput, Button } from 'react-native';
 
 import { StackActions } from '@react-navigation/native';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { teacherEditProfileDB } from '../store/actions'
+import { teacherEditProfileDB } from '../store/actions';
 import LottieView from 'lottie-react-native';
 
-function TeacherEditProfile({navigation}) {
-
+function TeacherEditProfile({ navigation }) {
   const dispatch = useDispatch();
-  const data = useSelector(state=>state);
+  const data = useSelector((state) => state);
 
   const { user } = data;
 
@@ -34,67 +27,95 @@ function TeacherEditProfile({navigation}) {
   }
 
   return (
-    <SafeAreaView>
-      {checkmark
-      ? <View style={stylesheet.checkmark}>
+    <SafeAreaView style={stylesheet.container}>
+      {checkmark ? (
+        <View style={stylesheet.checkmark}>
           <LottieView
             source={require('../assets/376-check-mark.json')}
-            onAnimationFinish={()=> navigation.dispatch(popAction)}// implement this instead of setimeout
-            style={{height:250,width:250, }}
+            onAnimationFinish={() => navigation.dispatch(popAction)} // implement this instead of setimeout
+            style={{ height: 250, width: 250 }}
             autoPlay //loop
             loop={false}
             speed={2}
-        />
-        <Text> Profile Saved!</Text>
+          />
+          <Text> Profile Saved!</Text>
         </View>
-      : <View>
-      <Text style={stylesheet.title}> {user.firstname} {user.lastname}</Text>
-      <TextInput
-        defaultValue={textDescription}
-        style={stylesheet.textbox}
-        onChangeText={text => setTextDescription(text)}
-        value={textDescription}
-        placeholder={'Tell your students about yourself!'}
-        multiline={true}
-        textAlign={'left'}
-        textAlignVertical={'top'}
-         
-      />
-      <View style={stylesheet.button}>
-        <Button
-          onPress={handleSubmit}
-          title="Done"
-          color="blue"
-        />
-      </View>
-      </View>}
+      ) : (
+        <View>
+          <Text style={stylesheet.title}>
+            {' '}
+            {user.firstname} {user.lastname}
+          </Text>
+          {/*  <View style={stylesheet.button}>
+            <Button onPress={handleSubmit} title="Done" color="blue" />
+          </View> */}
+          <TextInput
+            defaultValue={textDescription}
+            style={stylesheet.textbox}
+            onChangeText={(text) => setTextDescription(text)}
+            value={textDescription}
+            placeholder={'Tell your students about yourself!'}
+            multiline={true}
+            textAlign={'left'}
+            textAlignVertical={'top'}
+          />
+          <TouchableOpacity
+            style={stylesheet.button}
+            onPress={() => {
+              handleSubmit;
+            }}
+          >
+            <Text style={stylesheet.buttonText}>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </SafeAreaView>
   );
 }
 
 const stylesheet = StyleSheet.create({
+  container: { flex: 1, backgroundColor: '#FDAB8F', paddingTop: 100 },
   checkmark: {
-    paddingTop:40,
-    display:'flex',
+    paddingTop: 40,
+    display: 'flex',
     alignItems: 'center',
-    justifyContent:'center'
-
+    justifyContent: 'center',
   },
-  title:{
-    paddingTop:30,
-    padding:20,
+  title: {
+    paddingBottom: 50,
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+    alignSelf: 'center',
   },
   button: {
-    paddingTop:300
+    backgroundColor: 'rgba(206,212,211,0.5)',
+    alignSelf: 'center',
+    width: 300,
+    padding: 20,
+    borderColor: 'white',
+    borderRadius: 20,
+    marginBottom: 20,
+    borderWidth: 0.5,
+    alignItems: 'center',
   },
-  textbox:{
-    padding:10,
+  buttonText: {
+    color: 'black',
+    fontFamily: 'AvenirLTStdBook',
+    fontSize: 22,
+    // fontWeight: 'bold',
+  },
+  textbox: {
+    padding: 10,
     height: 240,
-    width:Dimensions.get('window').width,
-    borderColor: 'blue',
+    width: Dimensions.get('window').width - 50,
+    borderColor: '#E9967A',
+    borderRadius: 10,
     borderWidth: 2,
-    backgroundColor:'white'
-  }
+    alignSelf: 'center',
+    marginBottom: 30,
+    backgroundColor: 'white',
+  },
 });
 
 export default TeacherEditProfile;
