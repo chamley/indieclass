034e0c642d826165041c341deef8dfbc8160b014
ParisQZhang <parischang97@gmail.com>
2020-09-03 13:32:01 -0400

    Added Heroku database URL

--- Models/index.js
+++ Models/index.js
@@ -4,7 +4,12 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const db = {};
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const DATABASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'postgres://kixphaybpytuyj:b2f5381d74164da96378b1baaf4bac67ff4cbdcf029ca52fdca246019ea9bb9f@ec2-54-147-54-83.compute-1.amazonaws.com:5432/d7thb8au2mid99'
+    : process.env.DATABASE_URL;
+
+const sequelize = new Sequelize(DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
   pool: {
